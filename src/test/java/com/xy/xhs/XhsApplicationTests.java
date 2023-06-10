c416ee9aaed7d4358d94663b41928485bebdfabc
code4xy <2958253141@qq.com>
2023-06-10 21:10:25 +0800

    init

---
+++ src/test/java/com/xy/xhs/XhsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.xy.xhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XhsApplicationTests {
+
+  @Test
+  void contextLoads() {
+    System.out.println("hello world");
+  }
+
+}
